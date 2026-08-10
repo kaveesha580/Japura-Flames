a10007001c7d744115ce1f30516805775606927b
sadithsenanayake-blip <sadithsenanayake@gmail.com>
2026-08-10 21:06:07 +0530

    Registration Files

---
+++ Frontend/src/Registration/Registration.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Registration.css';
+
+function Registration() {
+  return (
+    <div className='loginGlassCard'>
+      <div className="registration-container">
+        {/* form comes here - next commits */}
+      </div>
+    </div>
+  );
+}
+
+export default Registration;
