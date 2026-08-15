9966945302854b392e8034772bfd5f8f61f24dd7
sheshmi1718 <sheshmichamodya30@gmail.com>
2026-08-15 23:29:47 +0530

    chore: initialize authentication router setup

---
+++ Backend/routes/auth.js
@@ -0,0 +1,8 @@
+//-------------------------------------
+// * Router set up for authentication
+//-------------------------------------
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
