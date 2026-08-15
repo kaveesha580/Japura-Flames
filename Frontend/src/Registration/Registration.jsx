c67334706bdeae061906c9bb9c692f9dfc9ba557
sadithsenanayake-blip <sadithsenanayake@gmail.com>
2026-08-15 06:57:51 +0530

    Add password and confirm password fields & Add client-side form validation

--- Frontend/src/Registration/Registration.jsx
+++ Frontend/src/Registration/Registration.jsx
@@ -45,6 +45,7 @@ function Registration() {
                     />
                     <i className="fa-solid fa-user input-icon"></i>
                   </div>
+                  {errors.fullName && <span className="error-message">{errors.fullName}</span>}
                 </div>
 
                 {/* Email */}
@@ -64,6 +65,7 @@ function Registration() {
                     />
                     <i className="fa-solid fa-envelope input-icon"></i>
                   </div>
+                  {errors.email && <span className="error-message">{errors.email}</span>}
                 </div>
 
                 {/* Phone */}
@@ -83,6 +85,7 @@ function Registration() {
                     />
                     <i className="fa-solid fa-phone input-icon"></i>
                   </div>
+                  {errors.phone && <span className="error-message">{errors.phone}</span>}
                 </div>
 
                 {/* Password */}
@@ -102,6 +105,7 @@ function Registration() {
                     />
                     <i className="fa-solid fa-lock input-icon"></i>
                   </div>
+                  {errors.password && <span className="error-message">{errors.password}</span>}
                 </div>
 
                 {/* Confirm Password */}
@@ -121,6 +125,7 @@ function Registration() {
                     />
                     <i className="fa-solid fa-check-circle input-icon"></i>
                   </div>
+                  {errors.confirmPassword && <span className="error-message">{errors.confirmPassword}</span>}
                 </div>
 
                 <div className="submit-section" style={{ gridColumn: '1 / -1' }}>
